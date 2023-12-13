3bb58d63147570d700a058dd0f4ceec3bd201c5c
tommy <tomas.nagy.tn@gmail.com>
2023-12-13 22:43:21 +0100

    chore: add reexports

--- src/magnitude.rs
+++
@@ -1,7 +0,0 @@
-use crate::phenotype::Phenotype;
-
-pub trait Magnitude<Pheno: Phenotype> {
-    fn magnitude(&self) -> f64;
-    fn min_magnitude(&self) -> f64;
-    fn max_magnitude(&self) -> f64;
-}
