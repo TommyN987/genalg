af211b23dd7bda8ee7db71bbb9f8e648b3b45698
tommy <tomas.nagy.tn@gmail.com>
2023-12-13 21:28:31 +0100

    feat: add generic to Magnitude

--- src/magnitude.rs
+++ src/magnitude.rs
@@ -1,4 +1,6 @@
-pub trait Magnitude {
+use crate::phenotype::Phenotype;
+
+pub trait Magnitude<Pheno: Phenotype> {
     fn magnitude(&self) -> f64;
     fn min_magnitude(&self) -> f64;
     fn max_magnitude(&self) -> f64;
